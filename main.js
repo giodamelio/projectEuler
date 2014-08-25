3aba6ad3a1506e8da85b0a21f582f35e16efd796
Gio d'Amelio <giodamelio@gmail.com>
2014-08-24 20:37:00 -0700

    Override log and print it to our output pane

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 /* jshint evil:true */
 var app = angular.module("projectEuler", ["ui.codemirror"]);
 
+// Override console.log to print to output pane
+(function () {
+    var old = console.log;
+    var logger = document.getElementById("log");
+    console.log = function (message) {
+        // Log it to the console
+        old.apply(this, arguments);
+
+        // Log it to the outpur pane
+        if (typeof message === "string" || typeof message === "number") {
+            logger.innerHTML += "<pre>" + message + "</pre>";
+        } else if (typeof message === "object") {
+            logger.innerHTML += "<pre>" + JSON.stringify(message, null, "    ") + "</pre>";
+        }
+
+        // Scroll the output pane
+        logger.scrollTop = logger.scrollHeight;
+    };
+})();
+
 // Script controller
 app.controller("ScriptController", ["$scope", function($scope) {
     // Our codemirror options
