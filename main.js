d5cf400e218b80fa8d10f502f3c7bf1ad3e7684e
Gio d'Amelio <giodamelio@gmail.com>
2014-08-24 19:40:45 -0700

    Run the current code

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
+/* jshint evil:true */
 var app = angular.module("projectEuler", ["ui.codemirror"]);
 
 // Script controller
 app.controller("ScriptController", ["$scope", function($scope) {
+    // Our codemirror options
     $scope.editorOptions = {
         theme: "monokai",
         mode: "javascript",
         lineWrapping : true,
         lineNumbers: true,
         indentUnit: 4
+    };
+
+    // Run the current code
+    $scope.run = function() {
+        eval($scope.code);
     };
 }]);
 
