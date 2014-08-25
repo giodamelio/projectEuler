7a6e856a2ad354a44325e4c3d9c79470fb2949a5
Gio d'Amelio <giodamelio@gmail.com>
2014-08-24 19:25:51 -0700

    Basic layout

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-var app = angular.module("projectEuler", []);
+var app = angular.module("projectEuler", ["ui.codemirror"]);
 
 // Script controller
 app.controller("ScriptController", ["$scope", function($scope) {
-    $scope.name = "Gio d'Amelio";
+    $scope.editorOptions = {
+        theme: "monokai",
+        mode: "javascript",
+        lineWrapping : true,
+        lineNumbers: true,
+        indentUnit: 4
+    };
 }]);
 
