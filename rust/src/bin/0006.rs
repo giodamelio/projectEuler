6285bd94e6cd170fb7f0e8656cd9e2327c67246f
Gio d'Amelio <giodamelio@gmail.com>
2016-05-13 11:24:35 -0700

    Rust: fix problem 6

--- rust/src/bin/0006.rs
+++ rust/src/bin/0006.rs
@@ -1,4 +1,5 @@
 #![feature(iter_arith)]
+#![feature(inclusive_range_syntax)]
 
 #[macro_use]
 extern crate math;
@@ -6,14 +7,14 @@ extern crate math;
 use std::iter::Iterator;
 
 fn main() {
-    let sum_of_squares: i64 = (1..100)
+    let sum_of_squares: i64 = (1...100)
         // Square the number
         .map(|n: i64| n.pow(2))
 
         // Sum them
         .sum();
 
-    let square_of_sum: i64 = (1..100)
+    let square_of_sum: i64 = (1...100)
         // Sum them
         .sum::<i64>()
 
